91cb9b95beb2af1c4cb6103cb67186e31ddcd6d7
PedroMReno <pedrohmreno@gmail.com>
2019-06-05 09:23:50 -0300

    Algoritimo de Busca Base

---
+++ GrafosDirigios/GrafosDirigios/Movimento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Movimento : IComparable<Movimento>
+{
+    private int cidade;
+    private int saida;
+    private int valor;
+
+    public Movimento(int cidade, int saida, int valor)
+    {
+        this.cidade = cidade;
+        this.saida = saida;
+        this.valor = valor;
+    }
+
+    public int Cidade { get => cidade; }
+    public int Saida { get => saida; }
+    public int Valor { get => valor; }
+
+    public override string ToString()
+    {
+        return "Cidade " + cidade;
+    }
+
+    public int CompareTo(Movimento other)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GrafosDirigios/GrafosDirigios/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int maxCidade = 11;
+        const int cidadeFinal = 3;
+        const int cidadeInicial = 6;
+        PilhaLista<Movimento> pilha = new PilhaLista<Movimento>();
+        List<int> todosAndados = new List<int>();
+        bool[] passouCidade = { false, false, false, false, false, false, false, false, false, false, false };
+        int[,] cidades = new int[, ] { { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, 100, 507, -1, -1, 230, -1, 180, -1, -1 },
+                                       { -1, 110, -1, 204, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, 120, -1, 97, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, 302, -1, -1, -1, 92, -1, -1, -1, -1 },
+                                       { -1, 88, 150, -1, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, -1, -1, 105, 113, -1, -1, -1, -1, 50 },
+                                       { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                                       { -1, -1, -1, -1, -1, -1, -1, -1, 74, 485, -1 }};
+        int cidadeAtual = cidadeInicial;
+        int saidaAtual = 0;
+
+        while (!(cidadeAtual == cidadeInicial && saidaAtual == maxCidade))
+        {
+            if(saidaAtual < maxCidade && cidadeAtual != cidadeFinal)
+            {
+                while (saidaAtual < maxCidade)
+                {
+                    if (passouCidade[saidaAtual] == false && cidades[cidadeAtual, saidaAtual] > -1)
+                        break;
+
+                    saidaAtual++;
+                }
+
+                if(saidaAtual != maxCidade)
+                {
+                    passouCidade[cidadeAtual] = true;
+                    pilha.Empilhar(new Movimento(cidadeAtual, saidaAtual, cidades[cidadeAtual, saidaAtual]));
+                    cidadeAtual = saidaAtual;
+                    saidaAtual = 0;
+                }
+            }
+            else
+            {
+                if(cidadeAtual == cidadeFinal)
+                {
+                    PilhaLista<Movimento> inver = new PilhaLista<Movimento>();
+                    inver.Empilhar(new Movimento(cidadeAtual, saidaAtual, 0));
+
+                    while (!pilha.EstaVazia())
+                        inver.Empilhar(pilha.Desempilhar());
+
+                    int totalAndado = 0;
+
+                    Write("Caminho encontrado: ");
+
+                    while (!inver.EstaVazia())
+                    {
+                        Movimento mov = inver.Desempilhar();
+                        totalAndado += mov.Valor;
+                        Write(mov + "; ");
+                        pilha.Empilhar(mov);
+                    }
+
+                    Write("Total andado: " + totalAndado);
+                    WriteLine();
+
+                    todosAndados.Add(totalAndado);
+                    pilha.Desempilhar();
+                }
+
+                passouCidade[cidadeAtual] = false;
+                Movimento m = pilha.Desempilhar();
+                cidadeAtual = m.Cidade;
+                saidaAtual = m.Saida + 1;
+            }
+        }
+
+        int menor = 0;
+        int maior = 0;
+
+        for(int i = 0; i < todosAndados.Count; i++)
+        {
+            if (todosAndados[i] < todosAndados[menor])
+                menor = i;
+
+            if (todosAndados[i] > todosAndados[maior])
+                maior = 1;
+        }
+
+        WriteLine("Menor caminho: " + (menor + 1));
+        WriteLine("Maior caminho: " + (maior + 1));
+
+        ReadLine();
+    }
+}
