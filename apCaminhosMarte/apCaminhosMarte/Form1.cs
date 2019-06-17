aa3f7bf61628d5d1a84ee1b6183306591291f970
AmabilePietrobon <42345581+AmabilePietrobon@users.noreply.github.com>
2019-06-17 10:24:48 -0300

    17/06
    relatorio
    comentario do forms

--- apCaminhosMarte/apCaminhosMarte/Form1.cs
+++ apCaminhosMarte/apCaminhosMarte/Form1.cs
@@ -13,11 +13,19 @@ namespace apCaminhosMarte
 {
     public partial class Form1 : Form
     {
+        // Armazenamento das cidades do arquivo texto
         ArvoreBinaria<Cidade> cidades;
+
+        //Armazenamento dos caminhos do arquivo texto
         CaminhoEntreCidades[,] caminhos;
+
+        //Armazenamento da menor rota calculada
         CalculadoraDeRotas calc;
+
+        //Armazenamento da lista de cidades (rota) selecionada
         List<Cidade> rotaSelecionada;
 
+        //Variaveis usadas para desenhar no PictureBox
         SolidBrush preenchimento = new SolidBrush(Color.Black);
         Pen caneta = new Pen(Color.Red, 3);
 
@@ -26,37 +34,41 @@ namespace apCaminhosMarte
             InitializeComponent();
         }
 
-        private void BtnBuscar_Click(object sender, EventArgs e)
+        //Codificacao do botao de busca de cainhos entre origem e destino selecionados
+        private void BtnBuscar_Click(object sender, EventArgs e) 
         {
-            //MessageBox.Show("Buscar caminhos entre cidades selecionadas");
-            if (lsbOrigem.SelectedIndex == lsbDestino.SelectedIndex)
+            if (lsbOrigem.SelectedIndex == lsbDestino.SelectedIndex) //Caso origem e destino sejam iguais, lanca uma excessao
                 MessageBox.Show("Selecione cidades diferentes para origem e destino.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             else
             {
+                //Lista de armazenamento de todos os caminhos para que seja alcancado o destino desejado
                 List<List<CaminhoEntreCidades>> rotas = calc.Calcular(lsbOrigem.SelectedIndex, lsbDestino.SelectedIndex);
+                //Lista de cidades componentes do menor caminho encontrado
                 List<CaminhoEntreCidades> melhorCaminho = new List<CaminhoEntreCidades>();
-                rotaSelecionada = new List<Cidade>();
-                dgvCaminhos.RowCount = 0;
-                dgvCaminhos.ColumnCount = 0;
+                rotaSelecionada = new List<Cidade>(); //Instaciacao da rota selecionada
+                dgvCaminhos.RowCount = 0; // zerando as linhas do grid
+                dgvCaminhos.ColumnCount = 0; // zerando as colunas do grid
                 int menor = int.MaxValue;
 
-                foreach (List<CaminhoEntreCidades> l in rotas)
+                foreach (List<CaminhoEntreCidades> l in rotas) // percorrendo a lista que contem a lista de caminhos
                 {
-                    dgvCaminhos.RowCount++;
-                    int andado = 0, colunaAtual = 0;
+                    dgvCaminhos.RowCount++; // adiciona uma linha ao grid
+                    int andado = 0, colunaAtual = 0; // variaveis locis para adicionar as cidades no grid
 
-                    if (l.Count() + 1 > dgvCaminhos.ColumnCount)
-                        dgvCaminhos.ColumnCount = l.Count() + 1;
+                    if (l.Count() + 1 > dgvCaminhos.ColumnCount) // se a quantidade de cidades for maior que a quantidade de colunas da tabela
+                        dgvCaminhos.ColumnCount = l.Count() + 1; // adiciona as colunas necessarias
 
-                    foreach(CaminhoEntreCidades dado in l)
+                    foreach(CaminhoEntreCidades dado in l) // percorre os caminhos da lista
                     {
-                        andado += dado.Distancia;
-                        dgvCaminhos[colunaAtual++, dgvCaminhos.RowCount - 1].Value = cidades.Buscar(new Cidade(dado.Origem)).ToString();
+                        andado += dado.Distancia; // soma a distancia a ja percorrida
+                        //escreve o ID e o nome da cidade no grid depois de busca-la na arvore
+                        dgvCaminhos[colunaAtual++, dgvCaminhos.RowCount - 1].Value = cidades.Buscar(new Cidade(dado.Origem)).ToString(); 
                     }
 
+                    // escreve o destino final no grid
                     dgvCaminhos[colunaAtual++, dgvCaminhos.RowCount - 1].Value = cidades.Buscar(new Cidade(lsbDestino.SelectedIndex)).ToString();
 
-
+                    //caso caminho percorrido seja menor que o ultimo que foi armazenado, ele e guardado
                     if (andado < menor)
                     {
                         menor = andado;
@@ -64,86 +76,90 @@ namespace apCaminhosMarte
                     }
                 }
 
-                if (melhorCaminho.Count > 0)
+                if (melhorCaminho.Count > 0) // se o melhor caminho foi encontrado
                 {
+                    // inicia o grid que armazena-o
                     dgvMelhorCaminho.ColumnCount = 1;
                     dgvMelhorCaminho.RowCount = 1;
 
-                    foreach (CaminhoEntreCidades dado in melhorCaminho)
+                    foreach (CaminhoEntreCidades dado in melhorCaminho) //percorre todos os caminhos da menor rota encontrada 
                     {
-                        Cidade atual = cidades.Buscar(new Cidade(dado.Origem));
-                        dgvMelhorCaminho[dgvMelhorCaminho.ColumnCount - 1, 0].Value = atual.ToString();
-                        dgvMelhorCaminho.ColumnCount++;
+                        Cidade atual = cidades.Buscar(new Cidade(dado.Origem)); //busca a cidade de origem do caminho em questao na arvore
+                        dgvMelhorCaminho[dgvMelhorCaminho.ColumnCount - 1, 0].Value = atual.ToString(); // escreve a ciadade no grid
+                        dgvMelhorCaminho.ColumnCount++; //diciona mais uma coluna ao grid
                     }
 
+                    // escreve o destino final no grid
                     dgvMelhorCaminho[dgvMelhorCaminho.ColumnCount - 1, 0].Value = cidades.Buscar(new Cidade(lsbDestino.SelectedIndex)).ToString();
                     ParaMostrarMelhorCaminho();
                 }
-                else
-                {
-                    dgvMelhorCaminho.ColumnCount = 0;
+                else //se o melhor caminho nao foi encontrado 
+                { 
+                    // zera o grid que o marmazenaria 
+                    dgvMelhorCaminho.ColumnCount = 0; 
                     dgvMelhorCaminho.RowCount = 0;
 
+                    // lanca uma excessao de caminho nao encontrado
                     MessageBox.Show("Nenhum caminho encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
 
-            pbMapa.Invalidate();
+            pbMapa.Invalidate(); // chama o refresh do PictureBox
         }
 
-        private void LerDeArquivo(bool ehCidade)
+        private void LerDeArquivo(bool ehCidade) // metodo usado para ler os dois arquivos que apenas recebe o boolean que indica se e cidade
         {
             if (dlgAbrir.ShowDialog() == DialogResult.OK) //Descobriremos o caminho do arquivo desejado
             {
-                StreamReader arq = new StreamReader(dlgAbrir.FileName);
+                StreamReader arq = new StreamReader(dlgAbrir.FileName); // abertura do arquivo
 
                 try
                 {
-                    while (!arq.EndOfStream)
+                    while (!arq.EndOfStream) // enquanto o arquivo nao terminou, percorremos 
                     {
                         if (!ehCidade)
                         {
-                            CaminhoEntreCidades c = new CaminhoEntreCidades(arq.ReadLine());
-                            caminhos[c.Destino, c.Origem] = c;
+                            CaminhoEntreCidades c = new CaminhoEntreCidades(arq.ReadLine()); // le o caminho do arquivo
+                            caminhos[c.Destino, c.Origem] = c; // armazena o caminho na matriz
                         }
-                        else
+                        else  // se for um arquivo de cidades
                         {
-                            Cidade c = new Cidade(arq.ReadLine());
-                            cidades.Incluir(c);
+                            Cidade c = new Cidade(arq.ReadLine()); // le a cidade do arquivo
+                            cidades.Incluir(c); // armazena a cidade na arvore 
                         }
                     }
                 }
-                catch
+                catch // caso arquivo nao esteja nos paroes de dados pre definidos, informa o usuario e chama novamente o metodo
                 {
                     MessageBox.Show("Por favor, selecione um arquivo válido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     LerDeArquivo(ehCidade);
                 }
 
-                arq.Close();
+                arq.Close(); // fecha o arquivo lido
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cidades = new ArvoreBinaria<Cidade>();
-            dlgAbrir.Title = "Ler cidades de: ";
+            cidades = new ArvoreBinaria<Cidade>(); // instancia a arvore
+            dlgAbrir.Title = "Ler cidades de: "; // coloca um titulo no dialog
 
-            LerDeArquivo(true);
+            LerDeArquivo(true); // chama o metodo de leitura de arquivo
 
-            dlgAbrir.Title = "Ler caminhos de: ";
-            caminhos = new CaminhoEntreCidades[cidades.QuantosFilhos, cidades.QuantosFilhos];
+            dlgAbrir.Title = "Ler caminhos de: "; // coloca outro titulo no mesmo dialog
+            caminhos = new CaminhoEntreCidades[cidades.QuantosFilhos, cidades.QuantosFilhos]; //instancia a matriz com a quiantidade de elementos da arvore
 
-            LerDeArquivo(false);
+            LerDeArquivo(false); //chama o metodo de leitura de arquivo
 
-            calc = new CalculadoraDeRotas(cidades.QuantosFilhos, caminhos);
-            rotaSelecionada = new List<Cidade>();
+            calc = new CalculadoraDeRotas(cidades.QuantosFilhos, caminhos); //instancia a calculadora de rotas com os caminhos e a quantidade de elementos da arvore
+            rotaSelecionada = new List<Cidade>(); // instancia a rota selecionada
 
-            cidades.ExecutaEmTodos((Cidade c) =>
+            cidades.ExecutaEmTodos((Cidade c) => //chama o metodo da arvore que recebe uma funcao como parametro e excutara essa funcao em todos os nos da arvore
             {
-                string mostrante = c.ToString();
+                string mostrante = c.ToString(); // recebe o nome da cidade
 
-                lsbOrigem.Items.Add(mostrante);
-                lsbDestino.Items.Add(mostrante);
+                lsbOrigem.Items.Add(mostrante); // escrve no listBox de origem
+                lsbDestino.Items.Add(mostrante);  // escrve no listBox de destino
             });
 
             // coloca os selecionados do ListBox em duas cidades diferentes
@@ -153,50 +169,58 @@ namespace apCaminhosMarte
 
         private void pbArvore_Paint(object sender, PaintEventArgs e)
         {
-            cidades.DesenharArvore(e.Graphics, pbArvore.Width);
+            cidades.DesenharArvore(e.Graphics, pbArvore.Width); // desenha a arvore com as cidades
         }
 
-        private void pbMapa_Paint(object sender, PaintEventArgs e)
+        private void pbMapa_Paint(object sender, PaintEventArgs e) //desenhar no mapa presente no pictureBox
         {
+            //instancia numeros base para o redimensionamento da imagem
             double redimenLargura = Math.Round((double)pbMapa.Width / 4096, 10);
             double redimenAltura = Math.Round((double)pbMapa.Height / 2048, 10);
-            Graphics grafs = e.Graphics;
             int tamanhoRedimen = Convert.ToInt32(35 * redimenLargura);
+            
+            Graphics grafs = e.Graphics;
 
-            cidades.ExecutaEmTodos((Cidade c) =>
+            cidades.ExecutaEmTodos((Cidade c) => // chama o metodo em todos os nos da arvore 
             {
-                int x = Convert.ToInt32(c.X * redimenLargura);
+                // redimensiona as coordenadas da cidade de acordo com o tamanho do mapa
+                int x = Convert.ToInt32(c.X * redimenLargura); 
                 int y = Convert.ToInt32(c.Y * redimenAltura);
 
-                grafs.FillEllipse(preenchimento, x, y, tamanhoRedimen, tamanhoRedimen);
 
-                string nome = c.Nome;
+                grafs.FillEllipse(preenchimento, x, y, tamanhoRedimen, tamanhoRedimen); // desenha um circulo nas coordenadas redimenionadas da cidade
+
+                string nome = c.Nome; // guarda o nome da cidade 
 
                 grafs.DrawString(nome, new Font("Courier New", tamanhoRedimen, FontStyle.Bold),
-                              new SolidBrush(Color.Black), x - (nome.Length * 4), y - 20);
+                              new SolidBrush(Color.Black), x - (nome.Length * 4), y - 20); // escreve o nome da cidade no mapa
             });
 
-            for (int i = 0; i < rotaSelecionada.Count - 1; i++)
+            for (int i = 0; i < rotaSelecionada.Count - 1; i++) // percorre a rota selecionada 
             {
-                Cidade origem = rotaSelecionada[i];
-                Cidade destino = rotaSelecionada[i + 1];
+                Cidade origem = rotaSelecionada[i]; // armazena a "cidade atual"na lista (for) 
+                Cidade destino = rotaSelecionada[i + 1];// armazena a proxima cidade
 
+                // se for uma das cidades perifericas que se ligam, tratamos diferente
                 if((origem.Nome == "Arrakeen" || origem.Nome == "Senzeni Na") && destino.Nome == "Gondor")
                 {
-                    int yNovo = 0;
+                    int yNovo = 0; // variavel local na nova coordenada y
 
                     if (origem.Nome == "Arrakeen")
-                        yNovo = 1000;
+                        yNovo = 1000; 
                     else
                         yNovo = 1350;
 
+
+                    // desenha a linha da cidade de rigem ate o novo ponto (na lateral do mapa para se liga a outra cidade na lateral oposta)
                     grafs.DrawLine(caneta, Convert.ToInt32(origem.X * redimenLargura) + tamanhoRedimen / 2, Convert.ToInt32(origem.Y * redimenAltura) + tamanhoRedimen / 2,
                         Convert.ToInt32(-50 * redimenLargura) + tamanhoRedimen / 2, Convert.ToInt32(yNovo * redimenAltura) + tamanhoRedimen / 2);
-
+                    
+                    // desenha a linha do novo ponto ate a cidade de destino (na lateral do mapa para se liga a outra cidade na lateral oposta)
                     grafs.DrawLine(caneta, Convert.ToInt32(4096 * redimenLargura) + tamanhoRedimen / 2, Convert.ToInt32(yNovo * redimenAltura) + tamanhoRedimen / 2,
                         Convert.ToInt32(destino.X * redimenLargura) + tamanhoRedimen / 2, Convert.ToInt32(destino.Y * redimenAltura) + tamanhoRedimen / 2);
                 }
-                else
+                else // se nao for, desenhamos a linha entre as cidades normalmente
                     grafs.DrawLine(caneta, Convert.ToInt32(origem.X * redimenLargura) + tamanhoRedimen/2, Convert.ToInt32(origem.Y * redimenAltura) + tamanhoRedimen / 2,
                         Convert.ToInt32(destino.X * redimenLargura) + tamanhoRedimen / 2, Convert.ToInt32(destino.Y * redimenAltura) + tamanhoRedimen / 2);
             }
@@ -206,36 +230,34 @@ namespace apCaminhosMarte
         {
             rotaSelecionada = new List<Cidade>();
 
-            for (int i = 0; i < dgvCaminhos.ColumnCount; i++)
+            for (int i = 0; i < dgvCaminhos.ColumnCount; i++) // percorrer a linha de rota selecionada
             {
-                if (dgvCaminhos[i, e.RowIndex].Value == null)
+                if (dgvCaminhos[i, e.RowIndex].Value == null) // se a celula atual for nula, sai da repeticao
                     break;
 
-                string mostrando = dgvCaminhos[i, e.RowIndex].Value.ToString();
-                Cidade busca = new Cidade(int.Parse(mostrando.Substring(0, mostrando.IndexOf('-'))));
-                Cidade lida = cidades.Buscar(busca);
-                rotaSelecionada.Add(lida);
+                string mostrando = dgvCaminhos[i, e.RowIndex].Value.ToString(); // guarda o nome da cidade da linha selecionada do grid
+                Cidade lida = cidades.Buscar(new Cidade(int.Parse(mostrando.Substring(0, mostrando.IndexOf('-'))))); // armazena o resultado da busca da cidade
+                rotaSelecionada.Add(lida); // adiciona a cidade lida a rota selecionada 
             }
 
-            pbMapa.Invalidate();
+            pbMapa.Invalidate(); // forca o paint do mapa
         }
 
         private void dgvMelhorCaminho_Click(object sender, EventArgs e)
         {
-            ParaMostrarMelhorCaminho();
-            pbMapa.Invalidate();
+            ParaMostrarMelhorCaminho(); //mostra o melhor caminho
+            pbMapa.Invalidate(); // forca o paint do mapa
         }
 
         private void ParaMostrarMelhorCaminho()
         {
-            rotaSelecionada = new List<Cidade>();
+            rotaSelecionada = new List<Cidade>(); // inicializa a rota selecionada
 
-            for (int i = 0; i < dgvMelhorCaminho.ColumnCount; i++)
+            for (int i = 0; i < dgvMelhorCaminho.ColumnCount; i++) // percorre o grid que guada o menor caminho 
             {
-                string mostrando = dgvMelhorCaminho[i, 0].Value.ToString();
-                Cidade busca = new Cidade(int.Parse(mostrando.Substring(0, mostrando.IndexOf('-'))));
-                Cidade lida = cidades.Buscar(busca);
-                rotaSelecionada.Add(lida);
+                string mostrando = dgvMelhorCaminho[i, 0].Value.ToString(); // guarda o ID da cidade 
+                Cidade lida = cidades.Buscar(new Cidade(int.Parse(mostrando.Substring(0, mostrando.IndexOf('-'))))); // armazena o retorno da busca na arvore
+                rotaSelecionada.Add(lida); // adiciona a cidade na rota
             }
         }
     }
